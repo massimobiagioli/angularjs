4cb9f5dd87a03ce24ed820629ee5fe7459e50522
massimo <github@massimobiagioli.it>
2013-11-06 14:32:28 +0100

    Completata visualizzazione da dati mock

--- ng-canvas/public_html/js/controllers.js
+++ ng-canvas/public_html/js/controllers.js
@@ -3,11 +3,22 @@
 angular.module('ngCanvasApp.controllers', []).
     controller('HomeController', ['$scope',  
         function($scope) {
-            
+            //TODO 
         }
     ]).
-    controller('ViewerController', ['$scope',  
-        function($scope) {
+    controller('ViewerController', ['$scope', 'DataLoaderFactory',  
+        function($scope, DataLoaderFactory) {
+            var data = DataLoaderFactory.loadMockData(),
+                layer;
             
+            $scope.stage = new Kinetic.Stage(data.stage);
+            
+            layer = new Kinetic.Layer();
+            
+            _.each(data.shapes, function(shapeData) {
+                layer.add(new Kinetic[shapeData.type](shapeData));
+            });
+            
+            $scope.stage.add(layer);
         }
     ]);
--- ng-canvas/public_html/js/services.js
+++ ng-canvas/public_html/js/services.js
@@ -1,3 +1,70 @@
 'use strict';
 
-angular.module('ngCanvasApp.services', []);
+angular.module('ngCanvasApp.services', []).
+    factory('DataLoaderFactory', function() {
+        return {
+            loadMockData: function() {
+                return {
+                    stage: {
+                        container: 'viewerContainer',
+                        width: 600,
+                        height: 400
+                    },
+                    shapes: [
+                        {
+                            type: 'Rect',
+                            x: 10,
+                            y: 10,
+                            width: 100,
+                            height: 50,
+                            fill: 'red',
+                            stroke: 'black',
+                            strokeWidth: 1,
+                            metadata: {
+                                cod: 'RECT_01',
+                                des: 'Primo rettangolo'
+                            }
+                        },
+                        {
+                            type: 'Rect',
+                            x: 120,
+                            y: 10,
+                            width: 100,
+                            height: 50,
+                            fill: 'green',
+                            stroke: 'black',
+                            strokeWidth: 1,
+                            metadata: {
+                                cod: 'RECT_02',
+                                des: 'Secondo rettangolo'
+                            }
+                        },
+                        {
+                            type: 'Circle',
+                            x: 280,
+                            y: 55,
+                            radius: 50,
+                            fill: 'yellow',
+                            stroke: 'black',
+                            strokeWidth: 2,
+                            metadata: {
+                                cod: 'CIRC_01',
+                                des: 'Primo cerchio'
+                            }
+                        },
+                        {
+                            type: 'Polygon',
+                            points: [340, 10, 450, 20, 490, 100, 450, 80, 400, 65, 340, 10],
+                            fill: 'blue',
+                            stroke: 'black',
+                            strokeWidth: 2,
+                            metadata: {
+                                cod: 'CIRC_02',
+                                des: 'Secondo cerchio'
+                            }                            
+                        }
+                    ]
+                };
+            }
+        };
+    });
