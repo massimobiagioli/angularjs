d2b382c4b8b48ffe37c284fe176bd6c98cd27507
massimobiagioli <github@massimobiagioli.it>
2013-12-14 22:21:15 +0100

    Completato esempio 1

--- ng-karma/app/js/controllers.js
+++ ng-karma/app/js/controllers.js
@@ -9,7 +9,25 @@ angular.module('ngKarmaApp.controllers', []).
     controller('EsempioController', ['$scope', 'EsempioFactory',  
         function($scope, EsempioFactory) {
             $scope.data = EsempioFactory.loadMockData();
-            console.log($scope.data);
+            $scope.filters = {};
+            
+            $scope.search = function(articolo) {
+                var result = true;
+                
+                if ($scope.filters.cod) {
+                    result = (-1 !== articolo.cod.indexOf($scope.filters.cod));
+                }
+                
+                if (result && $scope.filters.des) {
+                    result = (-1 !== articolo.des.indexOf($scope.filters.des));
+                }
+
+                if (result && $scope.filters.prezzo_unitario) {
+                    result = (articolo.prezzo_unitario <= $scope.filters.prezzo_unitario);
+                }
+
+                return result;
+            };
         }
     ]);
             
