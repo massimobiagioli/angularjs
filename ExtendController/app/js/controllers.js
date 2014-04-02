dacc57a67da40a8eb88fb0501205c9ddcb8f153c
massimo <github@massimobiagioli.it>
2014-04-02 14:01:54 +0200

    completato esempio

--- ExtendController/app/js/controllers.js
+++ ExtendController/app/js/controllers.js
@@ -3,9 +3,10 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+    controller('MyCtrl1', ["$scope", "BaseControllerFactory", 
+        function($scope, BaseControllerFactory) {
+            angular.extend(this, BaseControllerFactory.init($scope));
+        }])
+        .controller('MyCtrl2', [function() {
 
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+        }]);
--- ExtendController/app/js/services.js
+++ ExtendController/app/js/services.js
@@ -6,4 +6,16 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+    value('version', '0.1').
+    factory('BaseControllerFactory', function() {
+        return {
+            init: function($scope) {
+                $scope.num1 = 0;
+                $scope.num2 = 0;
+                $scope.result = 0;
+                $scope.sum = function() {
+                    $scope.result = $scope.num1 + $scope.num2;
+                };
+            }
+        };
+    });
